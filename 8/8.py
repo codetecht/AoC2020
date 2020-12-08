8cf2776e8fbe475e70143148703baa045d55aa45
codetecht <39983813+codetecht@users.noreply.github.com>
2020-12-07 22:24:40 -0800

    Adds day 8, gold complete; updates template

--- 8/8.py
+++ 8/8.py
@@ -4,31 +4,79 @@
 
 import re
 
-def main():
-    f = open("8/input.txt", "r+")
-    data = f.readlines()
+f = open("8/input.txt", "r+")
+data = f.readlines()
 
-    # For debugging
-    test_counter = 0
+code = {}
 
-    for line in data:
+for n, line in enumerate(data):
+    code[n] = line.rstrip()
 
-
-
-
-
-
-
-
-
-
-
-
-
-        # This part limits runtime for debugging
-        if test_counter > 3:
+def part1():
+    accum = 0
+    line_num = 0
+    executed = list()
+    repeated = False
+    
+    while not repeated:
+        if line_num in executed:
+            print("Accum: " + str(accum))
+            print(str(line_num) + ": " + code[line_num])
+            repeated = True
             break
-        else:
-            test_counter += 1
+        
+        executed.append(line_num)
+        
+        instr = code[line_num].split()[0]
+        value = code[line_num].split()[1]
+        
+        if instr == 'nop':
+            line_num += 1
+        elif instr == 'jmp':
+            line_num += int(value)
+        elif instr == 'acc':
+            accum += int(value)
+            line_num += 1
+    print(accum)
+            
+def part2():
+    edit_line = 0
+    
+    while edit_line < 640:
+        executed = list()
+        repeated = False
+        loop_lim = 0
+        line_num = 0
+        accum = 0
+        
+        while not repeated and loop_lim < 640:
+            if line_num in executed or line_num > 640:
+                if line_num > 640:
+                    print("EoC reached")
+                    print("Accum: " + str(accum))
+                    print(str(line_num) + ": " + code[line_num])
+                repeated = True
+                break
+            
+            executed.append(line_num)
+            
+            instr = code[line_num].split()[0]
+            value = code[line_num].split()[1]
+            if line_num == edit_line:
+                if instr == 'nop':
+                    instr = 'jmp'
+                elif instr == 'jmp':
+                    instr = 'nop'
+            
+            if instr == 'nop':
+                line_num += 1
+            elif instr == 'jmp':
+                line_num += int(value)
+            elif instr == 'acc':
+                accum += int(value)
+                line_num += 1
+                
+        edit_line += 1
 
-main()
+#part1()
+#part2()
