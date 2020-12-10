8a17b7f2968fa27fbc72c82d6c8ef88a51b90270
codetecht <39983813+codetecht@users.noreply.github.com>
2020-12-09 23:19:34 -0800

    Adds 10, gold star complete; adds 11, template

--- 10/10.py
+++ 10/10.py
@@ -1,33 +1,52 @@
-# Copyright 2020 Joseph Kularski
+# Copyright 2020 Joseph Kularski (gh: codetecht)
 # Workspace for Advent of Code 2020
 # Request permission before using or duplicating this code for any purpose
-
-import re
 
 f = open("10/input.txt", "r+")
 data = f.readlines()
 
-# For debugging
-test_counter = 0
-
+# Prep work
+adapters = [0]
 for line in data:
+    adapters.append(int(line))
+adapters.append(max(adapters)+3)
 
+# Sorting makes this much easier
+adapters = sorted(adapters)
 
+# Find the number of 1-jolt and 3-jolt jumps
+def part1(list):
+    single_j, triple_j = 0, 0
+    for n, el in enumerate(list[:-1]):
+        if list[n+1] - el == 1:
+            single_j += 1
+        elif list[n+1] - el == 3:
+            triple_j += 1
+    print(single_j*triple_j)
 
+# Find the number of paths to max joltage in list
+# This function starts from the *highest* value and works backward using a 
+#    dictionary. The number of paths to the highest joltage is set as the value
+#    to each adapter key. Traversing high-to-low, the number of "ways" from any
+#    arbitrary joltage up to the highest joltage is equal to the ways-sum of
+#    all adapters within reach (+3 jolts) of the current adapter.
+def part2(list):
+    paths = {}
+    list = sorted(list, reverse=True)
+    for n, el in enumerate(list):
+        # Add the initial max joltage entry
+        if len(paths) == 0:
+            paths[el] = 1
+        else:
+            i, ways = 1, 0
+            while (n-i >= 0) and (list[n-i] - el <= 3):
+                ways += paths[list[n-i]]
+                i += 1
+            paths[el] = ways
+    print(paths[min(paths)])
 
+def main():
+    part1(adapters)
+    part2(adapters)
 
-    # This part limits runtime for debugging
-    if test_counter > 3:
-        break
-    else:
-        test_counter += 1
-
-def part1():
-    foo = 'bar'
-    
-def part2():
-    bar = 'foo'
-    
-    
-#part1()
-#part2()
+main()
--- 9/9.py
+++ 9/9.py
@@ -4,16 +4,17 @@
 
 import re
 
+# Prep work
 f = open("9/input.txt", "r+")
 data = f.readlines()
 
+# Checks if any two element in the queue sum to value
 def checkQueue(value, queue):
     for i in queue:
         for j in queue:
             if i + j == value and queue.index(i) != queue.index(j):
                 return True
     return False
-
 
 def part1():
     queue = list()
@@ -23,13 +24,15 @@ def part1():
         if line != '\n':
             n = int(line.rstrip())
 
+            # Populate the initial queue
             if preamb > 25:
                 if checkQueue(n, queue) == False:
                     print(n)
                     return n
             else:
                 preamb += 1
-
+            
+            # First-in-first-out queue kept at length of 25
             queue.insert(0, n)
             while len(queue) > 25:
                 queue.pop()
@@ -39,16 +42,18 @@ def part2(broken_num):
         if line != '\n':
             sum = 0
             val_list = list()
+
+            # Check the list from the current number onward until sum >= broken_num
             for next_val in data[n:]:
                 next_val = int(next_val)
                 sum += next_val
                 val_list.append(next_val)
+                
+                # Victory condition
                 if sum == broken_num:
                     print(min(val_list) + max(val_list))
-                    return True
                 elif sum > broken_num:
                     break
-    return False
 
 def main():
     broken_num = part1()
